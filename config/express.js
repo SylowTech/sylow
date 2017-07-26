5b4c51818d97becec2efda52f7f26fce5c2767e6
Guillaume Couzy <guillaume.couzy@savoirfairelinux.com>
2017-07-26 15:24:17 -0400

    WIP

--- config/express.js
+++ config/express.js
@@ -44,6 +44,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+// WIP : wait for Server.getAuthoritative, use server uuid properly
+/*
+app.use((req, res, next) => {
+  res.setHeader('X-Sylow-Server', app.sylowServer);
+  next();
+});
+*/
 // secure apps by setting various HTTP headers
 app.use(helmet());
 
